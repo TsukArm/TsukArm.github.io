2d08a10952789db60e267198e211fcb4fca02efb
kembo <naoki@kembo.org>
2016-09-05 15:08:54 +0900

    ピリオド一文字いらなかった

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 $(function() {
   var addr = window.location.href.match(/\/[^\/]+$/);
-  var page_addr = "." + (addr ? addr[0] : "./index.html");
+  var page_addr = "." + (addr ? addr[0] : "/index.html");
 	$("#header-container").load("header.html", function() {
     $("#header-container li:has([href='" + page_addr + "'])").addClass("selected");
   });
